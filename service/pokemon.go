d1ae99a8a63fd9b19dc555b1ebac83d6d2790e33
Eugenio Garcia <eugenio.garcia@wizeline.com>
2022-02-09 17:58:22 -0600

    Add - csv read capabilites

--- service/pokemon.go
+++ service/pokemon.go
@@ -1,14 +1,18 @@
 package service
 
 import (
+	"encoding/csv"
+	"fmt"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/eugenio-garcia/Go-Dispatch-Bootcamp/model"
 )
 
 type PokemonMap map[int]model.Pokemon
 
-var pokemonsOrder []int = []int{1, 2}
+var pokemonsOrder []int = []int{1, 2, 3}
 
 var db PokemonMap = map[int]model.Pokemon{
 	1: {ID: 1, Name: "Pikachu"},
@@ -21,12 +25,65 @@ type PokemonService struct {
 
 func New(pm PokemonMap) *PokemonService {
 	if pm == nil {
-		pm = db
+		pm = OpenCSV()
 	}
 
 	return &PokemonService{
 		data: pm,
 	}
+}
+
+type PokemonRecord struct {
+	ID   int
+	Name string
+}
+
+func createPokemonMap(data [][]string) map[int]model.Pokemon {
+	var pokemonList PokemonMap = map[int]model.Pokemon{}
+	for i, line := range data {
+		if i > 0 { // omit header line
+			var rec PokemonRecord
+			for j, field := range line {
+				if j == 0 {
+					rec.ID, _ = strconv.Atoi(field) //Error?
+				} else if j == 1 {
+					rec.Name = field
+				}
+			}
+			pokemon := model.Pokemon{
+				ID:   rec.ID,
+				Name: rec.Name,
+			}
+			pokemonList[i] = pokemon
+		}
+	}
+	return pokemonList
+}
+
+func OpenCSV() map[int]model.Pokemon {
+	// open file
+	f, err := os.Open("data/pokemons.csv")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// remember to close the file at the end of the program
+	defer f.Close()
+
+	// read csv values using csv.Reader
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// convert records to array of structs
+	pokemonList := createPokemonMap(data)
+
+	// print the array
+	fmt.Printf("%+v\n", pokemonList)
+	return pokemonList
+
 }
 
 func (ps *PokemonService) GetAllPokemons() (model.Pokemons, error) {
